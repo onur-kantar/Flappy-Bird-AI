7ee6fcfc7125609a1bfa87c3ed5aaba6118dfb85
Onur Kantar <onurkantr@gmail.com>
2020-09-14 18:20:03 +0300

    the bird was trained

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class Bird : Agent
+{
+    [Header("References")]
+
+    [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private PipeHandler pipeHandler = null;
+
+    [Header("Settings")]
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float maxVelocityMagnitude = 5f;
+
+    private Vector3 startingPos;
+
+    public override void Initialize()
+    {
+        startingPos = transform.position;
+    }
+    public override void OnEpisodeBegin()
+    {
+        transform.position = startingPos;
+        rb.velocity = Vector3.zero;
+        pipeHandler.ResetPipes();
+    }
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        AddReward(0.1f);
+        if (Mathf.FloorToInt(vectorAction[0]) != 1) { return; }
+        jump();
+    }
+    //public override void Heuristic(float[] actionsOut)
+    //{
+    //    actionsOut[0] = 0;
+    //    if (!Input.GetKeyDown(KeyCode.Space)) { return; }
+    //    actionsOut[0] = 1;
+    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        AddReward(-1.0f);
+        EndEpisode();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            jump();
+        }
+    }
+    private void jump()
+    {
+        rb.velocity = Vector3.up * jumpForce;
+        //rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        //rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocityMagnitude);
+    }
+}
---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float speed = 5f;
+
+    void Update()
+    {
+        transform.Translate(new Vector3(-speed * Time.deltaTime, 0f, 0f));
+    }
+}
